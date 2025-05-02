dabf3b680927f681ec6a69245f2774bdef196cb0
Angelo Belchior <angelobelchior@hotmail.com>
2025-05-02 17:29:22 -0300

    Correção do executor de Notificações

--- TheMediator.Core/Executors/NotifierExecutor.cs
+++ TheMediator.Core/Executors/NotifierExecutor.cs
@@ -9,16 +9,19 @@ internal class NotifierExecutor(
     NotifierRegistry notifierRegistry) : IDisposable
 {
     private readonly ILogger logger = loggerFactory.CreateLogger(Constants.LogCategoryName);
-    
+
     public async Task PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken)
         where TMessage : notnull
     {
         var services = notifierRegistry.ListNotifiersByMessageType<TMessage>();
+        if (services.Count == 0) return;
+
         var channel = Channel.CreateBounded<Func<Task>>(new BoundedChannelOptions(services.Count));
-        
-        var sender = Task.Run(() => SendNotificationsAsync(message, services, channel, cancellationToken), cancellationToken);
+
+        var sender = Task.Run(() => SendNotificationsAsync(message, services, channel, cancellationToken),
+            cancellationToken);
         var receiver = Task.Run(() => ExecuteHandlersAsync(channel, cancellationToken), cancellationToken);
-        
+
         await Task.WhenAll(sender, receiver);
     }
 
@@ -36,12 +39,13 @@ internal class NotifierExecutor(
         {
             foreach (var serviceDescriptor in services)
             {
-                var handler = (INotificationHandler<TMessage>)serviceProvider.GetRequiredService(serviceDescriptor.MainType);
+                var handler =
+                    (INotificationHandler<TMessage>)serviceProvider.GetRequiredService(serviceDescriptor.MainType);
                 await channel.Writer.WriteAsync(() =>
                 {
                     logger.LogTrace("Sending message {MessageType} to {HandlerType}",
                         typeof(TMessage).Name, serviceDescriptor.MainType.Name);
-                
+
                     return handler.HandleAsync(message, cancellationToken);
                 }, cancellationToken);
             }
